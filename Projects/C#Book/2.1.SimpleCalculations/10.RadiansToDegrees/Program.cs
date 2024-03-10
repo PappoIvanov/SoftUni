75a24d74afd9fcfe5e70054db38097fa75542ed7
PappoIvanov <156020920+PappoIvanov@users.noreply.github.com>
2024-03-10 13:42:04 +0200

    Add tasks from 2.1. Simple Calculations Solution.

--- Projects/C#Book/2.1.SimpleCalculations/10.RadiansToDegrees/Program.cs
+++ Projects/C#Book/2.1.SimpleCalculations/10.RadiansToDegrees/Program.cs
@@ -6,7 +6,9 @@ namespace _10.RadiansToDegrees
     {
         static void Main(string[] args)
         {
-            
+            double radians = double.Parse(Console.ReadLine());
+            double degrees = Math.Round(radians * 180 / Math.PI);
+            Console.WriteLine(degrees);
         }
     }
 }
