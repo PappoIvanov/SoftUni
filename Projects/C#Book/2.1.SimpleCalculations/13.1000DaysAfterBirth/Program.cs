358387cfc47ec5c776076c553ce78537f98ecee4
PappoIvanov <156020920+PappoIvanov@users.noreply.github.com>
2024-03-10 13:51:21 +0200

    Add 13 task of 2.1.

---
+++ Projects/C#Book/2.1.SimpleCalculations/13.1000DaysAfterBirth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace _13._1000DaysAfterBirth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string birthDate = Console.ReadLine();
+            string format = "dd-MM-yyyy";
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            DateTime date = DateTime.ParseExact(birthDate, format, provider);
+            date = date.AddDays(1000);
+            Console.WriteLine("{0}", date.ToString(format));
+        }
+    }
+}
